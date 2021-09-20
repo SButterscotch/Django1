364426fca03a1a8ca84fdbf7ebe25a1e77d9df29
SButterscotch <ssambhavv@gmail.com>
2021-09-20 16:06:45 +0545

    Admin

--- SamSite/main/admin.py
+++ SamSite/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import TList, Item
 # Register your models here.
+
+admin.site.register(TList)
+admin.site.register(Item)
---
+++ SamSite/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="Home"),
+    path("<str:sname>", views.user, name="User"),
+    path("user/<int:id>", views.index, name="Index"),
+]
